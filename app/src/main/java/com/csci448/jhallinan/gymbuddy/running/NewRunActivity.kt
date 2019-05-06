0614dc7b61457f3e9ba8d80d3c68844fe49372fd
Josh <jhallinan@mymmail.mines.edu>
2019-05-05 18:33:44 -0600

    Running now tracks steps + distance and sends notification to screen. Need to add onclick listener to notification

--- app/src/main/java/com/csci448/jhallinan/gymbuddy/running/NewRunActivity.kt
+++ app/src/main/java/com/csci448/jhallinan/gymbuddy/running/NewRunActivity.kt
@@ -3,6 +3,8 @@ package com.csci448.jhallinan.gymbuddy.running
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
 
 import android.content.Context
 import android.content.Intent
@@ -13,21 +15,27 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Build
 import android.os.Bundle
+import android.os.SystemClock
 import android.support.v4.app.NotificationCompat
 import android.support.v4.app.NotificationManagerCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.util.Log
 import android.widget.Button
 import com.csci448.jhallinan.gymbuddy.R
+import com.csci448.jhallinan.gymbuddy.plans.WorkoutsRecyclerViewAdapter.Companion.LOG_TAG
 import kotlinx.android.synthetic.main.activity_new_run.*
 
 class NewRunActivity: AppCompatActivity(), SensorEventListener {
 
     private var isClockOn = false
+    private var isPaused = false
+    private var timeWhenPaused: Long = 0
 
     private var initialSteps: Int = 0
     private var stepsTaken: Int = 0
     private var distanceTraveled: Double = 0.0
+    private var baseTime: Long = 0
 
     private lateinit var sensorManager: SensorManager
     private lateinit var stepCounter: Sensor
@@ -36,13 +44,30 @@ class NewRunActivity: AppCompatActivity(), SensorEventListener {
     private lateinit var notificationManager: NotificationManager
     private lateinit var builder: NotificationCompat.Builder
 
+    private lateinit var resultIntent: Intent
+    private lateinit var resultPendingIntent: PendingIntent
+
+
     private fun updateSteps(){
         steps_taken.text = "$stepsTaken"
         distanceTraveled = stepsTaken / 2112.0
-        val distanceString = "%.2f".format(distanceTraveled)
-        distance_text_view.text = distanceString
+        distance_text_view.text = "%.2f".format(distanceTraveled)
+    }
 
+    private fun updateNotification() {
         builder.setContentText("${"%.2f".format(distanceTraveled)} miles    $stepsTaken steps taken")
+
+        resultIntent.putExtra(EXTRA_IS_CLOCK_ON, isClockOn)
+        resultIntent.putExtra(EXTRA_DISTANCE, distanceTraveled)
+        resultIntent.putExtra(EXTRA_STEPS_TAKEN, stepsTaken)
+        resultIntent.putExtra(EXTRA_INITIAL_STEPS, initialSteps)
+        resultIntent.putExtra(EXTRA_BASE_TIME, chronometer.base)
+
+        resultPendingIntent = TaskStackBuilder.create(this).run {
+            addNextIntentWithParentStack(resultIntent)
+            getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
         notificationManager.notify(NOTIFICATION_ID, builder.build())
     }
 
@@ -66,8 +91,31 @@ class NewRunActivity: AppCompatActivity(), SensorEventListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(LOG_TAG, "onCreate")
 
         setContentView(R.layout.activity_new_run)
+
+        // Intent values
+        isClockOn = intent.getBooleanExtra(EXTRA_IS_CLOCK_ON, false)
+        distanceTraveled = intent.getDoubleExtra(EXTRA_DISTANCE, 0.0)
+        stepsTaken = intent.getIntExtra(EXTRA_STEPS_TAKEN, 0)
+        initialSteps = intent.getIntExtra(EXTRA_INITIAL_STEPS, 0)
+        updateSteps()
+
+
+        baseTime = intent.getLongExtra(EXTRA_BASE_TIME, SystemClock.elapsedRealtime())
+
+        // If coming back from notification
+        if (isClockOn) {
+            chronometer.base  = baseTime
+            chronometer.start()
+
+            // Reveal stop & pause buttons
+            start_run_button.visibility = Button.INVISIBLE
+            stop_run_button.visibility = Button.VISIBLE
+            pause_run_button.visibility = Button.VISIBLE
+
+        }
 
         //Toolbar
         val toolbar = new_run_toolbar as Toolbar
@@ -77,6 +125,7 @@ class NewRunActivity: AppCompatActivity(), SensorEventListener {
         supportActionBar?.title = "Workouts"
         toolbar.setNavigationOnClickListener { finish() }
 
+        // Notifications
         notificationManager =
                 getSystemService(
                     Context.NOTIFICATION_SERVICE ) as NotificationManager
@@ -87,38 +136,65 @@ class NewRunActivity: AppCompatActivity(), SensorEventListener {
             "$stepsTaken steps taken"
         )
 
-        builder = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_run_notification)
-            .setContentTitle("Run in Progress")
-            .setContentText("${"%.2f".format(distanceTraveled)} miles    $stepsTaken steps taken")
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setOnlyAlertOnce(true)
+        resultIntent = Intent(this, NewRunActivity::class.java).apply {
+            flags = (Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        }
 
+        resultPendingIntent = TaskStackBuilder.create(this).run {
+            addNextIntentWithParentStack(resultIntent)
+            getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
+        builder = NotificationCompat.Builder(this, CHANNEL_ID).apply {
+            setSmallIcon(R.drawable.ic_run_notification)
+            setContentTitle("Run in Progress")
+            setContentText("${"%.2f".format(distanceTraveled)} miles    $stepsTaken steps taken")
+            priority = (NotificationCompat.PRIORITY_DEFAULT)
+            setContentIntent(resultPendingIntent)
+            setOnlyAlertOnce(true)
+        }
+
+        // Step Counter Sensor
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
 
         // Wire up buttons
         start_run_button.setOnClickListener {
-            isClockOn = !isClockOn
-            if (isClockOn){
-                start_run_button.isChecked = true
-                start_run_button.text = "Pause"
-                stop_run_button.visibility = Button.INVISIBLE
-                //start the clock
-                chronometer.start()
-                // create notification
-                with (NotificationManagerCompat.from(this)) {
+            isClockOn = true
+
+            if (isPaused) {
+                isPaused = false
+                chronometer.base = SystemClock.elapsedRealtime() + timeWhenPaused
+            }
+            else {
+                timeWhenPaused = 0
+                chronometer.base = baseTime
+            }
+
+            //start the clock
+            chronometer.start()
+
+            start_run_button.visibility = Button.INVISIBLE
+            stop_run_button.visibility = Button.VISIBLE
+            pause_run_button.visibility = Button.VISIBLE
+
+
+
+            // create notification
+            with (NotificationManagerCompat.from(this)) {
                     builder.setContentText("${"%.2f".format(distanceTraveled)} miles    $stepsTaken steps taken")
                     notify(NOTIFICATION_ID, builder.build())
                 }
-            }
-            else {
-                start_run_button.isChecked = false
-                start_run_button.text = "Start"
-                notificationManager.cancel(NOTIFICATION_ID)
-                //stop the clock
-                chronometer.stop()
-                stop_run_button.visibility = Button.VISIBLE
-            }
+        }
+
+        pause_run_button.setOnClickListener {
+            isPaused = true
+            notificationManager.cancel(NOTIFICATION_ID)
+            start_run_button.visibility = Button.VISIBLE
+            stop_run_button.visibility = Button.INVISIBLE
+            pause_run_button.visibility = Button.INVISIBLE
+
+            timeWhenPaused = chronometer.base - SystemClock.elapsedRealtime()
+            chronometer.stop()
         }
     }
 
@@ -132,8 +208,9 @@ class NewRunActivity: AppCompatActivity(), SensorEventListener {
     }
 
     override fun onPause() {
-        super.onPause()
+        updateNotification()
         sensorManager.unregisterListener(this)
+        super.onPause()
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
@@ -147,19 +224,29 @@ class NewRunActivity: AppCompatActivity(), SensorEventListener {
 
                     stepsTaken = (event.values[0]).toInt() - initialSteps
                     updateSteps()
+                    updateNotification()
                 }
-
             }
         }
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
 
-
     companion object {
-        private val CHANNEL_ID = "com.csci448.jhallinan.gymbuddy.running.steps"
+        private val LOG_TAG = "448.NewRunActivity"
 
+        // Notification Variables
+        private val CHANNEL_ID = "com.csci448.jhallinan.gymbuddy.running.steps"
         private val NOTIFICATION_ID = 1
+
+        // State Variables
+        private val EXTRA_IS_CLOCK_ON = "EXTRA_IS_CLOCK_ON"
+        private val EXTRA_DISTANCE = "EXTRA_DISTANCE"
+        private val EXTRA_STEPS_TAKEN = "EXTRA_STEPS_TAKEN"
+        private val EXTRA_ELAPSED_TIME = "EXTRA_ELAPSED_TIME"
+        private val EXTRA_INITIAL_STEPS = "EXTRA_INITIAL_STEPS"
+        private val EXTRA_BASE_TIME = "EXTRA_BASE_TIME"
+
         //How do I make me?
         fun createIntent(context: Context?) : Intent {
             val intent = Intent(context, NewRunActivity::class.java)
